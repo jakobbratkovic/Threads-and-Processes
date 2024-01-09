1021019bcdcbf36cd5073208e4178a8bdd525c9f
Jakob Bratkovic <git@shackfrere.net>
2024-01-09 20:02:44 +0100

    Print real rss usage instead of max and add a cleanup flag

--- proc.c
+++ proc.c
@@ -5,23 +5,48 @@
 #include <sys/resource.h>
 #include <string.h>
 #include <pthread.h>
+#include <sys/mman.h>
+#ifdef __APPLE__
+    #include <mach/mach.h>
+#endif
 
 #define SHORT_SLEEP 1
 #define LONG_SLEEP 3
 
 size_t allocBytes = 1024 * 1024 * 100;
+int cleanup = 0;
 
 void print_memory_usage(char *id) {
-    struct rusage r_usage;
-    getrusage(RUSAGE_SELF, &r_usage);
-    printf("%s:\t using memory %ld\n", id, r_usage.ru_maxrss);
+    long rss = 0;
+    #ifdef __APPLE__
+        struct task_basic_info t_info;
+        mach_msg_type_number_t t_info_count = TASK_BASIC_INFO_COUNT;
+        task_info(mach_task_self(), TASK_BASIC_INFO, (task_info_t)&t_info, &t_info_count);
+        rss = t_info.resident_size;
+    #elif defined __linux__
+        FILE* fd;
+        char token1[32];
+        char token2[32];
+        long pageSize = sysconf(_SC_PAGESIZE);
+        fd = fopen("/proc/self/statm", "r");
+        fscanf(fd, "%31s %31s", token1, token2);
+        rss = atoi(token2) * pageSize;
+    #endif
+    printf("%s:\t using memory %ld\n", id, rss);
 }
 
-void allocate_memory(char *id, size_t length) {
+void* allocate_memory(char *id, size_t length) {
     char *allocatedMemory;
     printf("%s\t allocating memory\n", id);
-    allocatedMemory = malloc(length);
+    allocatedMemory = mmap(0, length, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
     memset(allocatedMemory, 42, length);
+    return allocatedMemory;
+}
+
+void deallocate_memory(char *id, void *mem, size_t length) {
+    if(!cleanup) return;
+    printf("%s:\t deallocating memory\n", id);
+    munmap(mem, length);
 }
 
 void sleep_wrapper(char *id, int duration) {
@@ -30,11 +55,13 @@ void sleep_wrapper(char *id, int duration) {
 }
 
 void* thread_entry(void *arg) {
+    void *mem;
     printf("Thread:\t entry\n");
     sleep_wrapper("Thread", SHORT_SLEEP);
-    allocate_memory("Thread", allocBytes);
+    mem = allocate_memory("Thread", allocBytes);
     print_memory_usage("Thread");
     sleep_wrapper("Thread", LONG_SLEEP);
+    deallocate_memory("Thread", mem, allocBytes);
     printf("Thread:\t exit\n");
     return NULL;
 }
@@ -43,6 +70,21 @@ int main(int argc, char **argv) {
     pid_t processId;
     pid_t exitPid;
     pthread_t thread;
+    int opt;
+    int ch;
+
+    // free memory before exiting the thread when called with -c
+    while ((ch = getopt(argc, argv, "c")) != -1) {
+        switch (ch) {
+            case 'c':
+                cleanup = 1;
+                break;
+            case '?':
+            default:
+                return 0;
+            }
+    }
+    argc -= optind;
 
     printf("##### Forking - new process ######\n");
     printf("Forking\n");
